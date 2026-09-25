334170847338813226d0dc981e017e559764c6ca
ChekinNooget <aldenyuson@gmail.com>
2026-09-25 11:25:42 -0600

    clicking button rotates ring

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ var tiles = [
 	[false, false, false, false, false, false, false, false, false, false, false, false],
 ];
 
+var newStyles = document.createElement("style");
+
+var rotateOffset = 7;
+
+function rotateRing() {
+    rotateOffset++;
+    
+	var tiles = document.getElementsByClassName("tile");
+	for (let i = 0; i < tiles.length; i++) {
+        var newTransform = `rotateZ(${(rotateOffset + i)*360/12}deg) translateY(400%) rotateZ(-${(rotateOffset + i)*360/12}deg)`
+        tiles[i].style.transform = newTransform;
+	}
+}
