7d8b09d4f39f1985703ec10bc804e59465eddff1
ChekinNooget <aldenyuson@gmail.com>
2026-09-25 11:45:49 -0600

    rotate multiple rings

--- main.js
+++ main.js
@@ -11,14 +11,31 @@ var tiles = [
 
 var newStyles = document.createElement("style");
 
-var rotateOffset = 7;
+var rotateOffsets = [7, 7, 7, 7];
 
-function rotateRing() {
-    rotateOffset++;
+function rotateRing(ringIndex) {
+    rotateOffsets[ringIndex]++;
     
-	var tiles = document.getElementsByClassName("tile");
+	var tiles = document.getElementsByClassName("row")[ringIndex].children;
 	for (let i = 0; i < tiles.length; i++) {
-        var newTransform = `rotateZ(${(rotateOffset + i)*360/12}deg) translateY(400%) rotateZ(-${(rotateOffset + i)*360/12}deg)`
+        var newTransform = `rotateZ(${(rotateOffsets[ringIndex] + i)*360/12}deg) translateY(400%) rotateZ(-${(rotateOffsets[ringIndex] + i)*360/12}deg)`
         tiles[i].style.transform = newTransform;
 	}
+}
+
+function updateTileText(){
+    var grid = document.getElementById("grid");
+    for (let i = 0; i < grid.childElementCount; i++) {
+        for (let j = 0; j < grid.children[i].children.length; j++) {
+            grid.children[i].children[j].textContent = tiles[i][j];
+            console.log(grid.children[i].children[j]);
+        }
+    }
+}
+
+function setupStart(){
+    for (let i = 0; i < 4; i++) {
+        rotateRing(i);
+    }
+    updateTileText();
 }
