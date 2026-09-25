30dc78b387f77826eced204e98440e588ad99685
ChekinNooget <aldenyuson@gmail.com>
2026-09-25 08:27:48 -0600

    testing circle

---
+++ main.js
@@ -0,0 +1,11 @@
+//the level should be a 12x4 grid that visually wraps on the 4 end
+//level[x][y]
+//sliding a column should affect the one mod6 ahead of it, but upside down
+//sliding a row just slides the row
+var tiles = [
+	[false, false, false, false, false, false, false, false, false, false, false, false],
+	[false, false, false, false, false, false, false, false, false, false, false, false],
+	[false, false, false, false, false, false, false, false, false, false, false, false],
+	[false, false, false, false, false, false, false, false, false, false, false, false],
+];
+
